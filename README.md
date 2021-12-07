64de3c151394a962e45170975bc352c24bc00017
Stefan Suter <suter@mail.de>
2021-12-07 17:36:16 +0100

    Iteration 1 implemented and currencies in color

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Iteration 1:\
 As a user, I would like to be able to choose from which currency to which currency I want to convert. \
-I would like to enter the amount 1 and amount 2 and the computer will give me the converted result.\
+I would like to enter the amount and the computer will give me the converted result.\
 Fixed values are stored as the basis for calculation\
 Available currencies: Euro [EUR], US dollar [USD], Japanese yen [JPY], Australian dollar [AUD], British pound [GBP]
 
@@ -13,3 +13,6 @@ Daily values are taken from the Internet as the basis for calculation.
 Iteration 3:
 The converted result is saved in a file.\
 When converting again, the change to the previous conversion is also displayed in %.
+
+Iteration 4:
+After the conversion, the user is asked whether he would like to quit or continue.
--- main.py
+++ main.py
@@ -1,1 +1,70 @@
-print("Hello world")
+class TextCollor:
+    YELLOW = '\033[93m'
+    GREEN = '\033[92m'
+    RED = '\033[1;31m'
+    ENDC = '\033[0m'
+
+
+currencys = [
+    ['EUR', 'Euro'],
+    ['USD', 'US Dollar'],
+    ['JPY', 'Japanischer Yen'],
+    ['AUD', 'Australischer Dollar'],
+    ['GBP', 'Britischer Pfund']
+]
+
+conversion_values = [
+    # Euro
+    ['EUR/USD', 1.131],
+    ['EUR/JPY', 127.998],
+    ['EUR/AUD', 1.600],
+    ['EUR/GBP', 0.850],
+    # USD
+    ['USD/EUR', 0.884],
+    ['USD/JPY', 113.340],
+    ['USD/AUD', 1.417],
+    ['USD/GBP', 0.753],
+    # Japanese Yen
+    ['JPY/EUR', 0.885],
+    ['JPY/USD', 113.340],
+    ['JPY/AUD', 1.417],
+    ['JPY/GBP', 0.753],
+    # Australischer Dollar
+    ['AUD/EUR', 0.885],
+    ['AUD/USD', 113.340],
+    ['AUD/JPY', 1.417],
+    ['AUD/GBP', 0.753],
+    # Britisch Pound
+    ['JPY/EUR', 0.885],
+    ['JPY/USD', 113.340],
+    ['JPY/AUD', 1.417],
+    ['JPY/GBP', 0.753],
+]
+print(currencys[4][0])
+print(f'{TextCollor.YELLOW}Willkommen zum Währungsrechner!{TextCollor.ENDC}')
+
+print(f'Verfügbare {TextCollor.GREEN}Währungen{TextCollor.ENDC}: '
+          f'{currencys[0][1]} [{TextCollor.GREEN}{currencys[0][0]}{TextCollor.ENDC}],'
+          f'{currencys[1][1]} [{TextCollor.GREEN}{currencys[1][0]}{TextCollor.ENDC}],'
+          f'{currencys[2][1]} [{TextCollor.GREEN}{currencys[2][0]}{TextCollor.ENDC}],'
+          f'{currencys[3][1]} [{TextCollor.GREEN}{currencys[3][0]}{TextCollor.ENDC}],'
+          f'{currencys[4][1]} [{TextCollor.GREEN}{currencys[4][0]}{TextCollor.ENDC}]'
+      )
+
+from_currency = str.upper(input('Bitte gib die Währung ein, von der du umrechnen möchtest: '))
+to_currency = str.upper(input('Bitte gib die Währung ein, in die du umrechnen möchtest: '))
+
+merged_currencys_user_input = f'{from_currency}/{to_currency}'
+
+j = 0
+while j < len(conversion_values):
+    if conversion_values[j][0] == merged_currencys_user_input:
+        result = 0
+        print(f'Der Umrechnungskurs für {TextCollor.GREEN}{conversion_values[j][0]}{TextCollor.ENDC} '
+              f'beträgt: {TextCollor.GREEN}{conversion_values[j][1]}{TextCollor.ENDC}')
+        value = input('Gib bitte den Betrag ein, den du umrechnen möchtest: ')
+        result = f'Das Ergebnis beträgt: {TextCollor.GREEN}{int(value) * conversion_values[j][1]}{TextCollor.ENDC}' \
+                 f' {TextCollor.GREEN}{to_currency}{TextCollor.ENDC}'
+        print(result)
+        break
+    j += 1
